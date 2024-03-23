35bdb7a47e9b1bb19e0c9d4a1cf4eb26a861226f
Robert Brandl <rbrandl@stevens.edu>
2024-03-23 13:54:47 -0400

    Adding the git setup, including Readme, app.js, and a gitignore ifle

---
+++ app.js
@@ -0,0 +1,32 @@
+//Here is where you'll set up your server as shown in lecture code
+import express from 'express';
+const app = express();
+import configRoutes from "./routes/index.js"
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+import exphbs from 'express-handlebars';
+import session from 'express-session';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const staticDir = express.static(__dirname + '/public');
+
+app.use('/public', staticDir);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(session({
+  name: 'AuthState',
+  secret: 'some secret string!',
+  resave: false,
+  saveUninitialized: false
+}))
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
+});
