16c8bb787885b7649f28ad2384bde118159d7ade
Robert Brandl <rbrandl@stevens.edu>
2024-03-28 18:14:44 -0400

    Added setup for the bank and quiz

--- data/index.js
+++ data/index.js
@@ -1,4 +1,6 @@
 // This file should import both data files and export them as shown in the lecture code
 import userDataFunctions from './users.js';
+import bankDataFunctions from './bank.js';
 
 export const userData = userDataFunctions;
+export const bankData = bankDataFunctions;
--- routes/trivia.js
+++ routes/trivia.js
@@ -1,15 +1,18 @@
 import {Router} from 'express';
 const router = Router();
 router.route('').get(async (req, res) => {
-    //code here for GET will render the home handlebars file
+    //questions array: array of arrays (each element array has 5 elements: question, answer choice 1, 2, 3, 4)
+    let questions = [];
     if (req.session.user){
-        return res.render('trivia', {title: "Civics Trivia Quiz", notLoggedIn: false, firstName: req.session.user.firstName});
+        return res.render('trivia', {title: "Civics Trivia Quiz", notLoggedIn: false, firstName: req.session.user.firstName, questions: questions});
     }
     else{
-        return res.render('trivia', {title: "Civica Trivia Quiz", notLoggedIn: true});
+        return res.render('trivia', {title: "Civics Trivia Quiz", notLoggedIn: true, questions: questions});
     }
 })
 .post(async (req, res) => {
+    let correctAns = [];
+
 
 });
 export default router;
